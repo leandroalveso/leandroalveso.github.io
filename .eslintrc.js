35c325813ba2b0d3063627e5ebea2b681d2b1258
Leandro Alves <engleandroalveso@gmail.com>
2025-03-29 23:27:52 +1100

    fix github action pipe

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
       'next/core-web-vitals'
     ],
     rules: {
-      'react/no-unescaped-entities': 'off'
+      'react/no-unescaped-entities': 'off',
+      "@typescript-eslint/no-unused-vars": "warn"
     }
 };
